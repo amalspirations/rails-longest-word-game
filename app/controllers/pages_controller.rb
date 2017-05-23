389b03b53e638afd92a0d06e9f099add880ac30d
Amal Ketata <amal.ketata@edu.escpeurope.eu>
2017-05-23 01:02:18 +0100

    Game working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -17,7 +17,16 @@ class PagesController < ApplicationController
     @start_time = Time.parse(params[:time])
     @end_time = Time.now
     @result = run_game(@attempt, @grid, @start_time, @end_time)
-    render "pages/score"
+
+    session[:number] ||= 0
+    session[:number] += 1
+    @times = session[:number]
+
+    session[:scores] ||= []
+    session[:scores] << @result[:score]
+    @scores = session[:scores].reduce(:+) / session[:scores].length
+
+    # render "pages/score"
   end
 
 # ----- GAME CODE -----
@@ -36,9 +45,6 @@ class PagesController < ApplicationController
   end
 
   def repetition?(attempt, grid)
-    # test = true
-    # attempt.each_char { |char| test = test && attempt.count(char.upcase) <= grid.count(char) }
-    # return test
     attempt.split("").all? { |char| attempt.count(char) <= grid.count(char.upcase) }
   end
 
@@ -47,7 +53,7 @@ class PagesController < ApplicationController
     website = "https://api-platform.systran.net/translation/text/translate"
     response = open("#{website}?source=en&target=fr&key=#{key}&input=#{attempt}")
     json = JSON.parse(response.read.to_s)
-    translation = attempt != json["outputs"][0]["output"] ? json["outputs"][0]["output"] : nil
+    translation = attempt != json["outputs"][0]["output"] ? json["outputs"][0]["output"] : "-"
     return translation
   end
 
@@ -58,22 +64,22 @@ class PagesController < ApplicationController
 
   def message(attempt, grid)
     if !attempt_test?(attempt, grid)
-      return "not in the grid"
+      return "Fail! You used characters not in the grid..."
     elsif !english?(attempt)
-      return "not an english word"
+      return "Your attempt is not an English word!"
     elsif !repetition?(attempt, grid)
-      return "not in the grid"
+      return "Fail! You cannot repeat a character from the grid!"
     else
-      return "well done"
+      return "Well done!"
     end
   end
 
   def run_game(attempt, grid, start_time, end_time)
     result = {}
-    result[:time] = end_time - start_time
+    result[:time] = (end_time - start_time).to_i
     result[:translation] = translate(attempt)
     result[:message] = message(attempt, grid)
-    result[:score] = result[:message] == "well done" ? (attempt.length / result[:time].to_i) : 0
+    result[:score] = (result[:message] == "Well done!" ? (attempt.length * 1000 / result[:time]) : 0)
     return result
   end
 
